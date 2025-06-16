8eb315ad0b5fe8071f0a55ec2dc62ba3ae950371
seungdobae <svvwac98@saltware.co.kr>
2025-06-16 09:52:12 +0900

    fix: mody

--- managed-cluster/security.tf
+++ managed-cluster/security.tf
@@ -1,33 +1,36 @@
-# # ACM 인증서 발급 요청
-# resource "aws_acm_certificate" "service_domain" {
-#   domain_name       = "*.${local.service_domain_name}"
-#   validation_method = "DNS"
+# 기존 인증서 조회
+data "aws_acm_certificate" "existing" {
+  domain   = "${local.service_domain_name}"
+  statuses = ["ISSUED"]
+  most_recent = true
+}
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-
-# # 위에서 생성한 ACM 인증서 검증하는 DNS 레코드 생성
-# resource "aws_route53_record" "acm_validation_service_domain" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.service_domain.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
-
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.this.zone_id
-# }
-
+# 테스트 ACM 인증서 발급 요청
+resource "aws_acm_certificate" "test" {
+  domain_name       = "*.test.${local.service_domain_name}"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+# 위에서 생성한 ACM 인증서 검증하는 DNS 레코드 생성
+resource "aws_route53_record" "acm_validation_test_domain" {
+  for_each = {
+    for dvo in aws_acm_certificate.test.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.this.zone_id
+}
 # 인증서 발급 상태
-# resource "aws_acm_certificate_validation" "service_domain" {
-#   certificate_arn         = aws_acm_certificate.service_domain.arn
-#   validation_record_fqdns = [for record in aws_route53_record.acm_validation_service_domain : record.fqdn]
-# }
+resource "aws_acm_certificate_validation" "test" {
+  certificate_arn         = aws_acm_certificate.test.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation_test_domain : record.fqdn]
+}
